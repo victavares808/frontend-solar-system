5f7ca7f485ffa802c9c22e2655aac3ddbdd3bd88
OEM Configuration (temporary user) <Victor Tavares>
2022-02-18 16:34:08 -0300

    Primeiros requisitos, inicio de projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <>
+        <Header />
+        <>
+          <SolarSystem />
+          <Title />
+        </>
+      </>
+    );
   }
 }
-
 export default App;
