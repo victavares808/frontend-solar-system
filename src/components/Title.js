0e1d52de86b59c59736680a729b3777f79568a8d
OEM Configuration (temporary user) <Victor Tavares>
2022-02-18 20:29:42 -0300

    requisito4 corrigido

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import SolarSystem from './SolarSystem';
 
 class Title extends Component {
   render() {
     const { headline } = this.props;
     return (
-      <SolarSystem>
-        <h2>{headline}</h2>
-      </SolarSystem>
+      <h2>{headline}</h2>
     );
   }
 }
-
 Title.propTypes = { headline: PropTypes.string.isRequired };
 
 export default Title;
